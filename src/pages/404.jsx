6a5349fb8124667db20ecd8701c30e6401117ae3
Keivy Mora <mkeivy.mora@gmail.com>
2023-06-28 16:42:43 -0400

    test

---
+++ src/pages/404.jsx
@@ -0,0 +1,18 @@
+import { Box, Container } from "@mui/material";
+
+export const LoginPage = () => {
+  return (
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <h2>404 page not found...</h2>
+      </Box>
+    </Container>
+  );
+};
